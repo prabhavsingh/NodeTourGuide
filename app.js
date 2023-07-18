a42d852fed893031a5211963fb7ea29a837eec57
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2023-07-18 21:33:36 +0530

    Handling GET Requests

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
+//   res.status(200).json({
+//     message: "hello from server side!",
+//     app: "Natours",
+//   });
+// });
+
+// app.post("/", (req, res) => res.send("u can post to thi endpoint..."));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
-    message: "hello from server side!",
-    app: "Natours",
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
-
-app.post("/", (req, res) => res.send("u can post to thi endpoint..."));
 
 const port = 8000;
 app.listen(port, () => {
