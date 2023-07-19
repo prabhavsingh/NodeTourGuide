a2660bd84d1f1224f36d7669aded916b025b86fb
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2023-07-19 16:01:47 +0530

    Serving Static Files

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(morgon("dev"));
 
 //adds body data on req - data from the body is added to req object
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("hello from middleware 👋");
