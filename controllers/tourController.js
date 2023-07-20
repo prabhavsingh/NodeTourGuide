0a1dba7238fafe2d2a4f6026a4e48cbf5a228b0d
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2023-07-20 13:23:33 +0530

    Updating Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -56,13 +56,24 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<updated tourd here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'invalid data sent!',
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
