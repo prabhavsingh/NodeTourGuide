499250b09d0590add79bd975fa947019433ad3f9
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2023-07-24 01:35:18 +0530

    Protecting Tour Routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../model/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const app = require('../app');
 
 const signToken = (id) => {
   return jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -40,4 +41,26 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //1. getting token and check if its there
+  let token;
+  if (req.headers.authorization?.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401),
+    );
+  }
+  //2. verification token
+
+  //3.check if user still exists
+
+  //4, check if user change pasword after token was issued
+
+  next();
 });
