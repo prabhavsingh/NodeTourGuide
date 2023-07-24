ad1434b01b4d3ed6437298f61f68f1bddc944f96
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2023-07-25 01:59:43 +0530

    Password Reset Functionality_ Reset Token

--- model/userModel.js
+++ model/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -38,6 +39,8 @@ const userSchema = mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 userSchema.methods.correctPassword = async function (
@@ -58,6 +61,19 @@ userSchema.methods.changePasswordAfter = function (JWTTimestamp) {
   }
 
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return this.resetToken;
 };
 
 userSchema.pre('save', async function (next) {
