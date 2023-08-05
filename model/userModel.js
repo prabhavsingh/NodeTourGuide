d79daeee42bbcee4029173fc5949770f5532df49
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2023-08-06 03:36:21 +0530

    Deleting the Current User

--- model/userModel.js
+++ model/userModel.js
@@ -41,6 +41,11 @@ const userSchema = mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  actuve: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', async function (next) {
@@ -60,6 +65,12 @@ userSchema.pre('save', function (next) {
   this.passwordChangedAt = Date.now() - 1000;
   next();
 });
+
+userSchema.pre(/^find/, function (next) {
+  //this pints to current query
+  this.find({ active: { $ne: false } });
+  next()
+})
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
