d3b43ac8bdb8901279b7276d859af2d5cb18e04b
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2025-03-18 01:39:48 +0530

    Building Handler Factory Functions_ Delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,15 @@
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
-  Object.keys(obj).forEach(el => {
+  Object.keys(obj).forEach((el) => {
     if (allowedFields.includes(el)) newObj[el] = obj[el];
-  })
+  });
   return newObj;
-}
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -33,26 +34,29 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     );
   }
   //2. filter unwanted fields names that are not allowed to be updated
-  const filteredBody = filterObj(res.body, 'name', 'email')
+  const filteredBody = filterObj(res.body, 'name', 'email');
 
   //3. update user document
-  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true })
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
   res.status(200).json({
     status: 'success',
     data: {
-      user: updatedUser
-    }
+      user: updatedUser,
+    },
   });
 });
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
-  await User.findByIdAndUpdate(req.user.id, { active: false })
+  await User.findByIdAndUpdate(req.user.id, { active: false });
 
   res.status(204).json({
     status: 'success',
-    data: 'null'
-  })
-})
+    data: 'null',
+  });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
@@ -72,9 +76,5 @@ exports.updateUser = (req, res) => {
     message: 'this route is not yet defined!',
   });
 };
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined!',
-  });
-};
+
+exports.deleteUser = factory.deleteOne(User);
