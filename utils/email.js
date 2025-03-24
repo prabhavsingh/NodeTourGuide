186c7521e097ace59f76d277728f6c7a26856a85
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2025-03-24 21:48:40 +0530

    Building a Complex Email Handler

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,56 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { htmlToText } = require('html-to-text');
 
-const sendEmail = async (options) => {
-  //1. create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.nmae.split(' ')[0];
+    this.url = url;
+    this.from = `Prabhav <${process.env.EMAIL_FROM}>`;
+  }
 
-  //2. define the email options
-  const mailOptions = {
-    from: 'Jon Wick <john@hmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV.trim() === 'production') {
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  //3. actually send the email
-  await transporter.sendMail(mailOptions);
+  //send the actual email
+  async send(template, subject) {
+    //render HTML based on pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      },
+    );
+
+    //define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html),
+    };
+
+    //create transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    //send the actual email
+    await this.send('welcom', 'Welcome to TravelNest Family');
+  }
 };
-
-module.exports = sendEmail;
